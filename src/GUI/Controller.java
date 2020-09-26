621d90d6b6670ac641f412697a83ed7cc5b84885
Posampas <piotr.mierzejewski@mail.com>
2020-09-26 02:37:17 +0200

    Working progam bu with some flawas in the algoritm that have to be repaired in the next next versin

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package GUI;
 
 import Model.World;
 import javafx.animation.AnimationTimer;
@@ -6,8 +6,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
-
+import javafx.scene.paint.Color;
 
 
 public class Controller {
@@ -19,6 +21,7 @@ public class Controller {
     public void initialize(){
         world = new World(pane);
         timer = new Movement();
+        pane.setBackground(new Background(new BackgroundFill(Color.GREY,null,null)));
     }
 
     @FXML
@@ -32,7 +35,7 @@ public class Controller {
 
     @FXML
     public void randomButtonFunction(){
-            world.setGenerateRandomActiveCell();
+            /*world.setGenerateRandomActiveCell();*/
     }
 
     @FXML
@@ -49,28 +52,28 @@ public class Controller {
 
     @FXML
     public void setpButtonFucntion(){
+        step();
 
-        world.step();
     }
 
     @FXML
     Button allCellsActiveButton;
 
     public void allActiveButtonFuction(){
-        world.activateEveryCell();
+//        world.activateEveryCell();
     }
 
 
     @FXML
     public void startButtonAction(){
-        world.initializeCells(pane.getWidth(),pane.getHeight());
+        world.initializeCells(pane.getWidth());
     }
 
     @FXML
     public void getClik(MouseEvent event){
         double x = event.getX();
         double y = event.getY();
-        world.loopCellsFindTheOneClickeOn(x,y);
+        world.loopCellsFindTheOneClickedOn(x,y);
     }
 
     private class Movement extends AnimationTimer {
@@ -78,17 +81,17 @@ public class Controller {
         final long FRAMES_PER_SECOND = 50L;
         long INTERVAL = 1_00_000_000L / FRAMES_PER_SECOND;
         long last = 0;
-        int tick =0;
+//        int tick =0;
         @Override
         public void handle(long now) {
             if (now - last >= INTERVAL) {
                 last = now;
-                step(tick ++);
+                step();
             }
         }
     }
 
-    private void step(int tick){
+    private void step(){
         world.step();
     }
 }
--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package GUI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,12 +9,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
-    }
 
+
+    }
 
     public static void main(String[] args) {
         launch(args);
